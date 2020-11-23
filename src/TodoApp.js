6972a5cbd30b457a3ca67ed9b5835e0ddfc8a0e5
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-11-23 22:26:39 +0300

    Save to local storage

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,16 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TodoList from "./TodoList";
 import TodoForm from "./TodoForm";
 import { Typography, Paper, AppBar, Toolbar, Grid } from "@material-ui/core";
 import { v4 as uuid } from "uuid";
 
 function TodoApp() {
-  const initialTodos = [
-    { id: 1, task: "Walk the dog", completed: false },
-    { id: 2, task: "Grow beard", completed: true },
-    { id: 3, task: "Clean room", completed: false },
-  ];
+  const initialTodos = JSON.parse(window.localStorage.getItem("todos") || "[]");
   const [todos, setTodos] = useState(initialTodos);
+
+  useEffect(() => {
+    window.localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
   const addTodo = (newTodoText) => {
     setTodos([...todos, { id: uuid(), task: newTodoText, completed: false }]);
   };
--- src/TodoList.js
+++ src/TodoList.js
@@ -3,26 +3,28 @@ import Todo from "./Todo";
 import { Paper, List, Divider } from "@material-ui/core";
 
 function TodoList({ todos, removeTodo, toggleTodo, updateTodo }) {
-  return (
-    <Paper>
-      <List>
-        {todos.map((todo, i) => (
-          <>
-            <Todo
-              id={todo.id}
-              task={todo.task}
-              completed={todo.completed}
-              key={todo.id}
-              removeTodo={removeTodo}
-              toggleTodo={toggleTodo}
-              updateTodo={updateTodo}
-            />
-            {i < todos.length - 1 && <Divider />}
-          </>
-        ))}
-      </List>
-    </Paper>
-  );
+  if (todos.length)
+    return (
+      <Paper>
+        <List>
+          {todos.map((todo, i) => (
+            <>
+              <Todo
+                id={todo.id}
+                task={todo.task}
+                completed={todo.completed}
+                key={todo.id}
+                removeTodo={removeTodo}
+                toggleTodo={toggleTodo}
+                updateTodo={updateTodo}
+              />
+              {i < todos.length - 1 && <Divider />}
+            </>
+          ))}
+        </List>
+      </Paper>
+    );
+  return null;
 }
 
 export default TodoList;
